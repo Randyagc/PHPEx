f7bdc0c9faf387a0dd74e68198a8bbdfecd18622
Randy Granda <38142338+Randyagc@users.noreply.github.com>
2018-07-30 15:41:51 -0500

    Pantalla de Ingreso Sistema ASesores
    Programación inicial en HTML, para pantalla de ingreso al Sistema Asesores, en combinación JAVASCRIPT, CSS, y PHP

---
+++ Ingreso.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional //EN">
+<html>
+   <head>
+      <title> Ingreso SISTEMA ASESORES" </title>
+   </head>
+   <body>
+      <?php
+         ## Primera Tabla
+         echo "<table width=280 border=0 cellpadding=3 bgcolor=#1E679A>";
+         echo "<tr>";
+         echo "<td><font color=#FFFFFF face=Arial, verdana, helvetica>";
+         echo "<b>Recuadro curioso con HTML</b> </font> </td>";
+         echo "</tr>";
+         echo "<tr>";
+         echo "<td bgcolor=#ffffcc>";
+         echo "<font face=arial, verdana, helvetica>";
+         echo "Este es el interior del recuadro. Esperamos que os resulte elegante... es muy sencillo";
+         echo "</font> </td> </tr>";
+         echo "</table>";
+         echo "<br />";
+         ## Segunda Tabla
+         echo "<table width=200 cellspacing=1 cellpadding=3 border=0 bgcolor=#165480>";
+         echo "<tr> <td bgcolor=#5FA6D7>";
+         echo "<font size=1 face=verdana, arial, helvetica>";
+         echo "<b> Recuadro curioso con HTML </b>";
+         echo "</font> </td> </tr>";
+         echo "<tr> <td bgcolor=#ffffcc>";
+         echo "<font face=verdana, arial, helvetica, size=1>";
+         echo "Este es el interior del recuadro. Esperemos que resulte elegante... es muy sencillo";
+         echo "</font> </td> </tr> </table>";
+         echo "<br />";
+         ## Tercera Tabla
+         echo "<table width=200 cellspacing=1 cellpadding=3 border=0 bgcolor=#80A93E>";
+         echo "<tr>";
+         echo "<td bgcolor=#87F259><font size=1 face=verdana, arial, helvetica>";
+         echo "<b>Recuadro curioso con HTML</b> </font> </td>";
+         echo "</tr> <tr>";
+         echo "<td bgcolor=#F5EDB9>";
+         echo "<table width=95% cellspacing=1 cellpadding=1 border=0 align=center>";
+         echo "<tr> <td valign=top>";
+         echo "<font face=verdana, arial, helvetica size=1>+</font> </td>";
+         echo "<td> <font face=verdana, arial, helvetica size=1>";
+         echo "Opción uno";
+         echo "</font> </td> </tr>";
+         echo "<tr> <td valign=top> <font face=verdana, arial, helvetica size=1>+</font> </td>";
+         echo "<td> <font face=verdana, arial, helvetica size=1>";
+         echo "Otra opción con texto en varias líneas";
+         echo "</font> </td> </tr>";
+         echo "<tr> <td valign=top> <font face=verdana, arial, helvetica size=1>+</font> </td>";
+         echo "<td> <font face=verdana, arial, helvetica size=1>";
+         echo "Lo que sea que deses destacar";
+         echo "</font> </td> </tr>";
+         echo "<tr> <td valign=top> <font face=verdana, arial, helvetica size=1>+</font> </td>";
+         echo "<td> <font face=verdana, arial, helvetica size=1>";
+         echo "ültima opción";
+         echo "</font> </td> </tr> </table>";
+         echo "</td> </tr> </table>";
+      ?>
+   </body>
+</html>
